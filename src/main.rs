61849e50223806d4cea4f73ee91948a3fd916c74
Imod7 <dominique@imodworks.io>
2025-11-14 22:26:53 +0100

    save messages to db

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use colored::Colorize;
 use dotenv::dotenv;
 use reqwest::Client;
 use serde::Deserialize;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
 use std::env;
 use std::option::Option;
 
@@ -15,6 +17,7 @@ struct MessageDetails {
     text: Option<String>,
     chat: ChatMemberDetails,
     from: Option<User>,
+    date: i64,
 }
 
 #[derive(Debug, Deserialize)]
@@ -26,6 +29,7 @@ struct ChatMemberDetails {
 #[derive(Debug, Deserialize)]
 struct ChatMember {
     chat: ChatMemberDetails,
+    date: i64,
 }
 
 #[derive(Debug, Deserialize)]
@@ -43,10 +47,21 @@ struct TelegramResponse {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let api_key = env::var("API_KEY").expect("API_KEY must be set");
-    let bot_token: &str = api_key.as_str();
+    let bot_token = env::var("TELEGRAM_BOT_TOKEN").expect("API_KEY must be set");
+    let bot_token_string: &str = bot_token.as_str();
 
-    let tg_get_updates = format!("https://api.telegram.org/bot{}/getUpdates", bot_token);
+    let database_url =
+        env::var("DATABASE_URL").expect("DATABASE_URL environment variable is not set");
+    let pool: Pool<Postgres> = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+    println!("Successfully connected to the database!");
+
+    let tg_get_updates = format!(
+        "https://api.telegram.org/bot{}/getUpdates",
+        bot_token_string
+    );
 
     let client = Client::new();
     let response = client.get(tg_get_updates).send().await?;
@@ -59,64 +74,81 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response_as_text = response.text().await?;
     let response_data: TelegramResponse = serde_json::from_str(&response_as_text)?;
 
-    let mut i = 1;
-    let mut chat_ids = vec![];
-    let mut user_ids = vec![];
-    for data in response_data.result.iter() {
-        let chat_data = data.my_chat_member.as_ref();
-        if chat_data.is_some() {
-            let chat_details = chat_data.unwrap();
+    for (index, data) in response_data.result.iter().enumerate() {
+        let my_chat_member = data.my_chat_member.as_ref();
+        println!("\n#{}\nUpdateID: {}", index, data.update_id);
+        if my_chat_member.is_some() {
+            let chat_details = my_chat_member.unwrap();
             println!(
-                "{}{}\n  Update: {}, Chat_Id: {}, Chat_Title: {}",
-                " Message #".custom_color((99, 122, 185)),
-                i.to_string().custom_color((99, 122, 185)),
-                data.update_id,
-                chat_details.chat.id,
-                chat_details.chat.title,
+                "EntryType: {}\nChannel: {} - {}",
+                "my_chat_member ".custom_color((240, 168, 208)),
+                chat_details
+                    .chat
+                    .id
+                    .to_string()
+                    .custom_color((168, 251, 211)),
+                chat_details
+                    .chat
+                    .title
+                    .to_string()
+                    .custom_color((168, 251, 211)),
             );
+            let mut tx = pool.begin().await?;
+
+            sqlx::query(
+                r#"INSERT INTO updates (entry_type, chat_id, user_id, message_text, date) VALUES ($1, $2, $3, $4, $5)"#,
+            )
+            .bind("my_chat_member")
+            .bind(chat_details.chat.id)
+            .bind("none")
+            .bind("none")
+            .bind(chat_details.date)
+            .execute(&mut *tx)
+            .await?;
+            tx.commit().await?;
         } else {
             let message = data.message.as_ref();
+            let message_data = message.unwrap();
+
             if message.is_some() {
-                let msg_details = message.unwrap();
+                let message_text = message_data.text.as_deref().unwrap_or("No Text Available");
+                println!(
+                    "EntryType: {}\nText: {}",
+                    "message".custom_color((250, 218, 122)),
+                    message_text.custom_color((201, 104, 104))
+                );
 
-                let chat_id = msg_details.chat.id;
-                if !chat_ids.contains(&chat_id) {
-                    println!(
-                        "\n\nChannel: {}, {}",
-                        chat_id.to_string().custom_color((168, 251, 211)).bold(),
-                        msg_details
-                            .chat
-                            .title
-                            .to_string()
-                            .custom_color((168, 251, 211))
-                            .bold()
-                    );
-                    chat_ids.push(chat_id);
-                }
+                let chat_id = message_data.chat.id;
+                println!(
+                    "Channel: {}, {}",
+                    chat_id.to_string().custom_color((168, 251, 211)),
+                    message_data
+                        .chat
+                        .title
+                        .to_string()
+                        .custom_color((168, 251, 211))
+                );
 
-                let user = msg_details.from.as_ref();
+                let user = message_data.from.as_ref();
                 if user.is_some() {
                     let user_id = user.unwrap().id;
-                    if !user_ids.contains(&user_id) {
-                        println!(
-                            " User:{}",
-                            user_id.to_string().custom_color((79, 183, 179)).bold()
-                        );
-                        chat_ids.push(chat_id);
-                        user_ids.push(user_id);
-                    }
+                    println!("User: {}", user_id.to_string().custom_color((79, 183, 179)));
                 }
-                println!(
-                    "  {}{}\n   {}",
-                    " Message #".custom_color((99, 122, 185)),
-                    i.to_string().custom_color((99, 122, 185)),
-                    msg_details.text.as_deref().unwrap_or("None")
-                );
+                let mut tx = pool.begin().await?;
+
+                sqlx::query(
+                r#"INSERT INTO updates (entry_type, chat_id, user_id, message_text, date) VALUES ($1, $2, $3, $4, $5)"#,
+            )
+            .bind("message")
+            .bind(chat_id)
+            .bind("None")
+            .bind(message_text)
+            .bind(message_data.date)
+            .execute(&mut *tx)
+            .await?;
+                tx.commit().await?;
             }
         }
-        i += 1;
     }
-    println!("\nChats Vec: {:?}", chat_ids);
-    println!("Users Vec: {:?}", user_ids);
     Ok(())
 }
